9d05c3a34a300b96800225a18743e0429034d833
川林 <zhaoshuaibo.zsb@alibaba-inc.com>
2025-06-23 10:10:58 +0800

    unittest

--- src/alibaba_cloud_ops_mcp_server/alibabacloud/utils.py
+++ src/alibaba_cloud_ops_mcp_server/alibabacloud/utils.py
@@ -1,9 +1,41 @@
 from alibabacloud_credentials.client import Client as CredClient
 from alibabacloud_tea_openapi.models import Config
+from fastmcp.server.dependencies import get_http_request
+
+
+def get_credentials_from_header():
+    try:
+        request = get_http_request()
+        headers = request.headers
+        access_key_id = headers.get('x-ak-proven-access-key-id', None)
+        access_key_secret = headers.get('x-ak-proven-access-key-secret', None)
+        token = headers.get('x-acs-ak-proven', None)
+        credentials = None
+        if access_key_id:
+            credentials = {
+                'AccessKeyId': access_key_id,
+                'AccessKeySecret': access_key_secret,
+                'SecurityToken': token
+            }
+        return credentials
+    except RuntimeError:
+        # 没有活跃的HTTP请求上下文时返回None
+        return None
 
 
 def create_config():
-    credentialsClient = CredClient()
-    config = Config(credential=credentialsClient)
+    credentials = get_credentials_from_header()
+    if credentials:
+        access_key_id = credentials.get('AccessKeyId', None)
+        access_key_secret = credentials.get('AccessKeySecret', None)
+        token = credentials.get('SecurityToken', None)
+        config = Config(
+            access_key_id=access_key_id,
+            access_key_secret=access_key_secret,
+            security_token=token
+        )
+    else:
+        credentialsClient = CredClient()
+        config = Config(credential=credentialsClient)
     config.user_agent = 'alibaba-cloud-ops-mcp-server'
     return config
